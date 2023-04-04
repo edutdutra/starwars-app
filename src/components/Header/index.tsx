6fbf13323889c157806ea6b8087b0327d5476bf8
Eduardo-Dutra <edutdutra@gmail.com>
2023-04-04 08:59:39 -0300

    add header go back navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
 import {Image, TouchableOpacity, View} from "react-native";
 import {AntDesign} from "@expo/vector-icons";
 
-import logoPng from "../../assets/logo.png"
+import {useNavigation} from "@react-navigation/native";
+import {AppNavigatorRoutesProps} from "../../routes/app.routes";
 
+import logoPng from "../../assets/logo.png"
 import {styles} from "./styles";
 
 export function Header() {
+    const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+    function handleGoBack() {
+        navigation.goBack();
+    }
+
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.backButton}>
+            <TouchableOpacity style={styles.backButton} onPress={handleGoBack}>
                 <AntDesign name="leftcircleo" size={24} color="black"/>
             </TouchableOpacity>
 

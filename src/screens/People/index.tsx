ce1cff5dc4da1ca569456158b83076912d724eab
Eduardo-Dutra <edutdutra@gmail.com>
2023-04-04 08:34:27 -0300

    add flat list in people screen

--- src/screens/People/index.tsx
+++ src/screens/People/index.tsx
@@ -1,21 +1,34 @@
 import {FlatList, Text, View} from "react-native";
 
 import {Header} from "../../components/Header";
-
-import {styles} from "./styles";
 import {Title} from "../../components/Title";
 import {PeopleCard} from "../../components/PeopleCard";
 
+import {styles} from "./styles";
+
 export function People() {
+    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+
     return (
         <View style={styles.container}>
-            <Header />
-            <Title title="Pessoas" subtitle="De todos os filmes do Star Wars" />
+            <Header/>
+            <Title title="Pessoas" subtitle="De todos os filmes do Star Wars"/>
 
-            <PeopleCard />
-            <PeopleCard />
-            <PeopleCard />
-            <PeopleCard />
+            <View style={styles.listContent}>
+                {/*ajustar quando puxar os dados*/}
+                <FlatList
+                    data={data}
+                    renderItem={({item}) => (
+                        <PeopleCard/>
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    ListEmptyComponent={() => (
+                        <Text style={styles.listEmptyText}>
+                            Nenhum pessoa encontrada
+                        </Text>
+                    )}
+                />
+            </View>
         </View>
     )
 }
--- src/screens/People/styles.ts
+++ src/screens/People/styles.ts
@@ -6,13 +6,19 @@ export const styles = StyleSheet.create({
         flex: 1,
         padding: 24,
         alignItems: 'center',
+        paddingBottom: 200
     },
     logo: {
         width: 300,
         height: 120,
         marginTop: 10
     },
-    menu: {
-        marginTop: 180
+    listContent: {
+        marginTop: 100
+    },
+    listEmptyText: {
+        color: '#1B1D1E',
+        fontSize: 14,
+        textAlign: 'center'
     }
 });
